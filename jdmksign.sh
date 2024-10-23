5932c20a55f00066ff2fc9e36bceec9b790807de
thisThink <106603221+thisThink@users.noreply.github.com>
2024-10-23 13:00:57 +0800

    Add files via upload

---
+++ jdmksign.sh
@@ -0,0 +1,6 @@
+curl --location --request GET 'http://dl2.jdmk.xyz:1170/user/signin/thisPunk' \
+--header 'Cookie: satoken=23f3fd6e-7946-4303-83ea-7d24f2c8bbf1' \
+--header 'User-Agent: Apifox/1.0.0 (https://apifox.com)' \
+--header 'Accept: */*' \
+--header 'Host: dl2.jdmk.xyz:1170' \
+--header 'Connection: keep-alive'
